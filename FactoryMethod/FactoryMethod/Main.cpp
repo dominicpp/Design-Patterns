224bd1f81d306fbd34dc66998195b5d0609c0a61
Dominic Engel <dominic.engel@student.htw-berlin.de>
2023-08-10 15:05:46 +0200

    added example for factory method

--- FactoryMethod/FactoryMethod/Main.cpp
+++ FactoryMethod/FactoryMethod/Main.cpp
@@ -1,6 +1,77 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+// abstract class
+class Animal
+{
+public:
+	virtual void makeSound() = 0;
+};
+
+// concrete animal class
+class Lion : public Animal
+{
+public:
+	void makeSound() override
+	{
+		std::cout << "Lion Roars\n";
+	}
+};
+
+// concrete animal class
+class Elephant : public Animal
+{
+public:
+	void makeSound() override
+	{
+		std::cout << "Elephant Trumpets\n";
+	}
+};
+
+// abstract factory
+class EnclosureCreator
+{
+public:
+	virtual Animal* createAnimal() = 0;
+};
+
+// concrete enclosure creator for lions
+class LionEnclosureCreator : public EnclosureCreator 
+{
+	Animal* createAnimal() override
+	{
+		return new Lion();
+	}
+};
+
+// concrete enclosure creator for elephants
+class ElephantEnclosureCreator : public EnclosureCreator
+{
+	Animal* createAnimal() override
+	{
+		return new Elephant();
+	}
+};
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	std::vector<EnclosureCreator*> enclosures;
+
+	enclosures.push_back(new LionEnclosureCreator());
+	enclosures.push_back(new ElephantEnclosureCreator());
+
+	for (EnclosureCreator* enclosure : enclosures)
+	{
+		Animal* animal = enclosure->createAnimal();
+		animal->makeSound();
+		delete animal;
+	}
+
+	for (EnclosureCreator* enclosure : enclosures) 
+	{
+		delete enclosure;
+	}
+
+	return 0;
 }
